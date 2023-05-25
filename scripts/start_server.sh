3561b1ec598b2fe0ff2f63a6e316a29c6a896971
Panainte Diana <95704033+dnadnaywy@users.noreply.github.com>
2023-05-25 22:41:41 +0300

    Update start_server.sh

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # navigate to app folder
-cd /usr/share/nginx/html/
+cd /home/ec2-user/server
 
 # initial startup by running react-script "start", name process "marketing"
 # --watch watches and restarts if files change
