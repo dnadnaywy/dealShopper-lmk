0d6fdaf038bff294a138f794383dfd78ebc1fb38
Panainte Diana <95704033+dnadnaywy@users.noreply.github.com>
2023-05-25 23:27:44 +0300

    Update before_install.sh

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -4,5 +4,5 @@
 cd /home/ec2-user/server
 
 # install node and npm
-curl -sL https://rpm.nodesource.com/setup_14.x | sudo -E bash -
-yum -y install nodejs npm
+# curl -sL https://rpm.nodesource.com/setup_14.x | sudo -E bash -
+sudo yum -y install nodejs npm
